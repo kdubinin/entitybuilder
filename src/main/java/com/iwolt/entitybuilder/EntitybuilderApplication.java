94b2afd53e436c65f9221b037497c73ceb72a3aa
Kiryl Dubinin <dubinin@us.ibm.com>
2020-10-08 13:36:02 +0300

    first commit

---
+++ src/main/java/com/iwolt/entitybuilder/EntitybuilderApplication.java
@@ -0,0 +1,13 @@
+package com.iwolt.entitybuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitybuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntitybuilderApplication.class, args);
+    }
+
+}
