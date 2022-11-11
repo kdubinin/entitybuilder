222eafe8501525a5fa94b7d1c5e983b42e6515ab
Kiryl Dubinin <kdubinin@ibaazristoutlook.onmicrosoft.com>
2022-11-11 12:11:47 +0100

    Simple boot app

--- src/main/java/com/iwolt/entitybuilder/EntitybuilderApplication.java
+++ src/main/java/com/iwolt/entitybuilder/EntitybuilderApplication.java
@@ -2,10 +2,13 @@ package com.iwolt.entitybuilder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.iwolt.entitybuilder.repo")
+@EntityScan("com.iwolt.entitybuilder.model")
 @SpringBootApplication
 public class EntitybuilderApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EntitybuilderApplication.class, args);
     }
---
+++ src/main/java/com/iwolt/entitybuilder/repo/BookRepository.java
@@ -0,0 +1,10 @@
+package com.iwolt.entitybuilder.repo;
+
+import com.iwolt.entitybuilder.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByTitle(String title);
+}
